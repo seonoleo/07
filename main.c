97ea08940667b6be566f4defca9ede8542299324
seonoleo <mscomy1@gmail.com>
2025-10-16 15:26:57 +0900

    02

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-    int flag =1;
-    int y;
-    while(flag!=0)
+    int i;
+    int temp=1;
+    
+    for(i=0;i<5;i++)
     {
-        y=3;
-        flag=0;
+       printf("temp=%d\n",temp);
+       temp++;
     }
-    y=4;
   
   system("PAUSE");	
   return 0;
